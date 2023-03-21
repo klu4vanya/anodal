67b1a1e83be647b6cc99944f8f9954061f8fd1e0
klu4vanya <86849458+klu4vanya@users.noreply.github.com>
2023-03-21 20:19:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-var more = document.getElementsByClassName("mores");
-  function readMore(){
-     if (event.target.className = "btns") {
-         //alert(event.target.id);
-         if (event.target.innerText === "• Торцевание") {
-             more[0].style.display = "inline-block";
-             event.target.innerHTML = "Скрыть";
-         } else {
-             more[0].style.display = "none";
-             event.target.innerHTML = "• Торцевание";
-         }
-     }
- }
- 
-  function readMore2() {
-     if (event.target.className = "btns") {
-         //alert(event.target.id);
-         if (event.target.innerText === "• Вырубка") {
-             more[1].style.display = "inline-block";
-             event.target.innerHTML = "Скрыть";
-         } else {
-             more[1].style.display = "none";
-             event.target.innerHTML = "• Вырубка";
-         }
-     }
- }
- function readMore3() {
-     if (event.target.className = "btns") {
-         //alert(event.target.id);
-         if (event.target.innerText === "• Сверление") {
-             more[2].style.display = "inline-block";
-             event.target.innerHTML = "Скрыть";
-         } else {
-             more[2].style.display = "none";
-             event.target.innerHTML = "• Сверление";
-         }
-     }
- }
+
+ function scrollToElement() {
+    var element = document.getElementById("about");
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+  }
+  function scrollToElement_tg(){
+    var element = document.getElementById("tg");
+    element.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+  }
+  function scrollToElement_contacts(){
+    var element = document.getElementById("contacts");
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+  }
+  function scrollToElement_mec(){
+    var element = document.getElementById("mec-res");
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+  }
+  function scrollToElement_production(){
+    var element = document.getElementById("production-title");
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+  }
+
+
+
+
+
+
+
